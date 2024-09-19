28bc9c62d3b6273a65754412cd9b000b2fb501d4
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-09-19 08:59:04 +0200

    fix(DB): db name is too generic

--- meditask-be/src/database/database.providers.ts
+++ meditask-be/src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://localhost/test2'),
+      await mongoose.connect('mongodb://localhost/meditask'),
   },
 ];
