7ed09e0785f6f10f5d45f4d9097a7b5e825add27
Tomas Kraus <tomas.kraus@usetreno.cz>
2024-10-21 16:19:16 +0200

    refactor(FE): TaskProvider, provides context for task components

--- frontend/mt-app/src/App.js
+++ frontend/mt-app/src/App.js
@@ -1,53 +1,19 @@
 import './App.css';
-import { useReducer } from 'react'
 import { ItemList } from './ItemList'
-
-const data = [
-  {
-    "_id": "670925f3f582f10ffd3c5ab0",
-    "caption": "string",
-    "description": "string",
-    "startDate": "2024-09-11T00:00:00.000Z",
-    "endDate": "2024-09-11T00:00:00.000Z",
-    "schedule": "\"0 30 11 * * 1-5\" Monday to Friday at 11:30am",
-    "__v": 0
-  },
-  {
-    "_id": "67092776f582f10ffd3c5acd",
-    "caption": "Helou",
-    "description": "word",
-    "startDate": "2024-09-11T00:00:00.000Z",
-    "endDate": "2024-09-11T00:00:00.000Z",
-    "schedule": "0 45 11 * * 1-5",
-    "__v": 0
-  }
-]
+import { TaskProvider } from './TasksProvider';
 
 function App() {
-
-  const [tasks, dispatch] = useReducer(taskReducer, data)
 
   return (
     <div className='App'>
       <h1>Medical Tasks</h1>
-      <>
-        <ItemList items={tasks} dispatch={dispatch} />
-      </>
+      <TaskProvider>
+        <ItemList />
+      </TaskProvider>
     </div>
   );
 }
 
-
-const taskReducer = (tasks, action) => {
-  switch (action.type) {
-    case 'deleted_task': {
-      return tasks.filter(t => t._id !== action.id)
-    }
-    default: {
-      throw new Error(`Unknown task action type: ${action.type}`)
-    }
-  }
-}
 
 export default App;
 
--- frontend/mt-app/src/ItemList.js
+++ frontend/mt-app/src/ItemList.js
@@ -1,7 +1,12 @@
 import MyButton from './MyButton'
 import TaskItem from './TaskItem';
+import { TasksContext, TaskDispatchContext } from './TasksProvider';
 
-export function ItemList({ items = [], dispatch }) {
+import { useContext } from 'react';
+
+export function ItemList() {
+    const items = useContext(TasksContext)
+    const dispatch = useContext(TaskDispatchContext)
     const listItems = items.map((item) =>
         <li key={item._id}>
             <TaskItem {...item} />
